986b7c709452c786ec95c04f5ebcc4551f588a80
PashkaSm <smolynets_pavlo@ukr.net>
2020-07-14 11:08:22 +0200

    Not

--- db.php
+++
@@ -1,6 +0,0 @@
-<?php 
-$dbhost = "localhost";
-$dbname = "retro-eko-loft";
-$username = "retro-eko-loft";
-$password ="QAW123sed456";
-$db = new PDO("mysql:host=$dbhost; dbname=$dbname",$username ,$password);
